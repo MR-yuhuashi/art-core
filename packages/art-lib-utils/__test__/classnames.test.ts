a0b9d3a4e8a7d111eb659ba57afc7af6049bac33
qnn-front-mac <zhangxiangyu@kuainiugroup.com>
2019-10-29 14:39:31 +0800

    add test case for classnames.ts and add test coverage

--- packages/art-lib-utils/__test__/classnames.test.ts
+++ packages/art-lib-utils/__test__/classnames.test.ts
@@ -1,33 +1,59 @@
 import { classNames } from '../src/utils/classnames';
+import { isString } from 'util';
 
 /**
- * 返回class name
- * 参数可以是string，number，object（只一层），array(可嵌套多层)
- * 不可是boolean null undefined，但可以是Boolean的包装对象
+ * 返回一个string
+ * 入参的个数和类型：基本类型和引用类型
  */
-describe('params are string or number', () => {
-    test('', () => {
-        expect(classNames(1, 2, 3, 'a')).toBe('1 2 3 a');
-    });
-});
-describe('params contains array or object', () => {
-    test('object has one nesting', () => {
-        expect(classNames(1, [2, 3], 'a', {b: 'sss'})).toBe('1 2 3 a b');
-    });
-    test('array has more than one nesting', () => {
-        expect(classNames(1, [2, 3], [[4, 5]], 'a', [[6, [7, 8]]])).toBe('1 2 3 4 5 a 6 7 8');
-    });
-    test('object has more than one nesting', () => {
-        expect(classNames(1, [2, 3], [[4, 5]], 'a', {b: 'sss', c: {d: 'www'}})).toBe('1 2 3 4 5 a b c');
-    });
-});
-describe('params contains boolean or null or undefined', () => {
-    test('need an object', () => {
-        expect(classNames('a', [2, 3], Number(1))).toBe('a 2 3 1');
-    });
-    test('boolean or null or undefined', () => {
-        expect(classNames(1, [2, 3], true)).toBe('1 2 3');
-        expect(classNames(1, [2, 3], null)).toBe('1 2 3');
-        expect(classNames(1, [2, 3], undefined)).toBe('1 2 3');
-    });
+
+describe('classnames', () => {
+  test('pass some valid params and the result will be a string', () => {
+    const result = classNames('active', 'btn', 2, '');
+    expect(isString(result)).toBeTruthy();
+  });
+
+  test('pass valid params and result will be joined with empty space', () => {
+    expect(classNames('active', 'btn', '')).toBe('active btn');
+  });
+
+  test('boolean params true and false both will be skipped', () => {
+    expect(classNames(true, false, 'active')).toBe('active');
+  });
+
+  test('string params empty string will be skipped', () => {
+    expect(classNames('', 'active', 'btn')).toBe('active btn');
+  });
+
+  test('number params 0 will be skipped', () => {
+    expect(classNames(+0, -0, 1, -1, 'active', 'btn')).toBe('1 -1 active btn');
+  });
+
+  test('undefined and null will be skipped', () => {
+    expect(classNames(undefined, null, 'active', 'btn')).toBe('active btn');
+  });
+
+  test('array params will be joined', () => {
+    expect(classNames(['active', 'btn1'], 'btn2', ['btn3', 'btn4'])).toBe('active btn1 btn2 btn3 btn4');
+  });
+
+  test('deep array params will also be joined', () => {
+    expect(classNames(['active', ['btn1', 'btn2', ['btn3', 'btn4']]], 'btn5')).toBe('active btn1 btn2 btn3 btn4 btn5');
+  });
+
+  test('object params will join the truthy key', () => {
+    expect(classNames({active: 1, btn1: true, btn2: '1', btn3: undefined}, 'btn4')).toBe('active btn1 btn2 btn4');
+  });
+
+  test('not support deep object params and will skip the children key', () => {
+    expect(classNames({active: 1, btn1: true, btn2: '1', btn3: { a: 1, b: 0 }}, 'btn4')).toBe('active btn1 btn2 btn3 btn4');
+  });
+
+  test('support any type params and will join together', () => {
+    expect(classNames(
+      { active: 1, btn1: true, btn2: '1', btn3: { a: 1, b: 0 } },
+      'btn4',
+      [undefined, [1, 2], ['a', 'b']],
+      0))
+    .toBe('active btn1 btn2 btn3 btn4 1 2 a b');
+  });
 });
